00dac9d4afa6c6add3c66ecefc5356c691fc5d93
Amatriciana <amatriciana913@gmail.com>
2022-04-20 20:36:58 +0900

    i

--- lib/database.dart
+++ lib/database.dart
@@ -1,1 +1,33 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
+class AppDatabase {
+  late Database _database;
+
+  Future<Database> get database async {
+    _database = await _initDB();
+    return _database;
+  }
+
+  Future<Database> _initDB() async {
+    String path = join(await getDatabasesPath(), 'form.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createTable,
+    );
+  }
+
+  Future<void> _createTable(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE forms(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        form1 INTEGER,
+        form2 INTEGER,
+        result INTEGER,
+        is_done BOOLEAN,
+      )
+    ''');
+  }
+}
