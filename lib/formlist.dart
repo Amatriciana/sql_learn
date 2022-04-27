6ab6034bde379f2d231cbc8974408283cad6f229
Amatriciana <amatriciana913@gmail.com>
2022-04-28 08:33:28 +0900

    listview作成

--- lib/formlist.dart
+++ lib/formlist.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class FormListPage extends StatelessWidget {
+class FormListPage extends HookConsumerWidget {
   const FormListPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('aa'),
-    );
+  Widget build(BuildContext context, WidgetRef ref) {
+    final list = [
+      ['1', '2', '3']
+    ];
+
+    return Scaffold(
+        body: ListView.builder(
+      itemCount: list.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Dismissible(
+            key: UniqueKey(),
+            child: Column(
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        width: 1,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                  child: ListTile(
+                    title: Text('${list[index][0]}'
+                        '${list[index][1]}'
+                        '${list[index][2]}'),
+                  ),
+                ),
+              ],
+            ));
+      },
+    ));
   }
 }
